02c3356ae3efb414512d3a593bb777a3b3cb374a
andrewdpetersen <andrew.petersen@revature.net>
2021-09-29 08:58:45 -0500

    p0 update

--- project-0/FullProject0/src/main/java/menus/validuser/BankMenu.java
+++ project-0/FullProject0/src/main/java/menus/validuser/BankMenu.java
@@ -12,9 +12,12 @@ import utils.MyArrayList;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class BankMenu extends LoginMenu {
     public void bankMenu(Users user) throws SQLException, IOException {
+
+        //MENU OPTIONS
 
         //display accounts list
         Connection conn = ConnectionManager.getConnection();
@@ -33,9 +36,36 @@ public class BankMenu extends LoginMenu {
         }
         System.out.println("The total balance of all your accounts is: $" + totalBalance);
 
-        //MENU OPTIONS
         //create new bank account
+        Connection conn = ConnectionManager.getConnection();
+        AccountsDAO dao = new AccountsDAO(conn);
+
+        Accounts account = new Accounts();
+        System.out.println("Please select the type of account you would like to create:\n" +
+                "(1) Savings\n" +
+                "(2) Checking\n" +
+                "(3) Business");
+        Scanner scan = new Scanner(System.in);
+        String accountTypeSelection = scan.nextLine();
+
+        if(accountTypeSelection.equals("1")){
+            account.setAccount_type("Savings");
+        }else if(accountTypeSelection.equals("2")){
+            account.setAccount_type("Checking");
+        }else if(accountTypeSelection.equals("3")){
+            account.setAccount_type("Business");
+        }else {
+            //no account type selected
+        }
+
+        account.setBalance(0.0f);
+        account.setUser_id(user.getUser_id());
+
+        dao.updateAccounts(account);
+
         //view account
+
         //logout-quit -> OuterMenu
+
     }
 }
